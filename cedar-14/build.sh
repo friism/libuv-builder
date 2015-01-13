d5ae9c663da2e81bdf1049978e115129cc864a5c
Michael <friism@gmail.com>
2015-01-13 15:55:28 -0800

    libuv no longer in joyent GH org

--- cedar-14/build.sh
+++ cedar-14/build.sh
@@ -1,7 +1,7 @@
 set -e
 
 if [ ! -f "/var/cache/libuv-$VERSION.tar.bz2" ]; then
-  wget -q -O /var/cache/libuv-$VERSION.tar.gz https://github.com/joyent/libuv/archive/v${VERSION}.tar.gz
+  wget -q -O /var/cache/libuv-$VERSION.tar.gz https://github.com/libuv/libuv/archive/v${VERSION}.tar.gz
 fi
 
 tar zxf /var/cache/libuv-$VERSION.tar.gz
